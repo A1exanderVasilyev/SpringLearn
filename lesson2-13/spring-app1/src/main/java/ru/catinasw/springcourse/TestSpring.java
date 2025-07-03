92bc0b13190790a83d4ce5ba62bd530a8e9a93e4
Catina Swag <catinasw@un-h1.kzn.21-school.ru>
2025-07-03 16:30:57 +0300

    make lesson2-13

--- lesson2-13/spring-app1/src/main/java/ru/catinasw/springcourse/TestSpring.java
+++ lesson2-13/spring-app1/src/main/java/ru/catinasw/springcourse/TestSpring.java
@@ -1,17 +1,23 @@
 package ru.catinasw.springcourse;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(SpringConfig.class);
 
-        MusicPlayer musicPlayer = context.getBean(MusicPlayer.class);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
 
         ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
