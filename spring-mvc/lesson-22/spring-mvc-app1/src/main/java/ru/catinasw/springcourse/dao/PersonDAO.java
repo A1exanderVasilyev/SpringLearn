16ed925cfe86faf29b5f640e0650d054dfced7a3
Catina Swag <catinasw@un-h1.kzn.21-school.ru>
2025-07-04 17:12:51 +0300

    make lesson-22: implemented get requests of people and person info from model with implemention of corresponding views

---
+++ spring-mvc/lesson-22/spring-mvc-app1/src/main/java/ru/catinasw/springcourse/dao/PersonDAO.java
@@ -0,0 +1,33 @@
+package ru.catinasw.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.catinasw.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PERSON_ID;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PERSON_ID, "Tom"));
+        people.add(new Person(++PERSON_ID, "Bob"));
+        people.add(new Person(++PERSON_ID, "Mike"));
+        people.add(new Person(++PERSON_ID, "Kate"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream()
+                .filter(person -> person.getId() == id)
+                .findAny()
+                .orElse(null);
+    }
+
+}
