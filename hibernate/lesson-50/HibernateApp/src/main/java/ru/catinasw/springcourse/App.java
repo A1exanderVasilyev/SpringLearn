d9c79b3058d95fe4105c0cdb109bf23c72a4ba66
Catina Swag <catinasw@va-i2.kzn.21-school.ru>
2025-07-09 13:00:17 +0300

    make lesson-50: write hibernate methods for update, delete entities

---
+++ hibernate/lesson-50/HibernateApp/src/main/java/ru/catinasw/springcourse/App.java
@@ -0,0 +1,31 @@
+package ru.catinasw.springcourse;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.catinasw.springcourse.model.Person;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+            Person person = session.find(Person.class, 2);
+            session.remove(person);
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
