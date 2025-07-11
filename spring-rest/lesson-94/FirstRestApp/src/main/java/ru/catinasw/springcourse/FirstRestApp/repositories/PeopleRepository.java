b509ce21a1ca784c8ee753f00713bbe101c620c2
Catina Swag <catinasw@va-i2.kzn.21-school.ru>
2025-07-11 14:12:23 +0300

    prepared for task

---
+++ spring-rest/lesson-94/FirstRestApp/src/main/java/ru/catinasw/springcourse/FirstRestApp/repositories/PeopleRepository.java
@@ -0,0 +1,10 @@
+package ru.catinasw.springcourse.FirstRestApp.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.catinasw.springcourse.FirstRestApp.models.Person;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+}
