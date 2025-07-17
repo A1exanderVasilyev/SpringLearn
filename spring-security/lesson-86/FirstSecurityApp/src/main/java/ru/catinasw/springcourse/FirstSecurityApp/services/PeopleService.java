b51bcd8fb3c92e2d6f52549c40c605824628e443
Catina Swag <catinasw@va-i2.kzn.21-school.ru>
2025-07-17 15:45:28 +0300

    make lesson-86: implemented roles for users

---
+++ spring-security/lesson-86/FirstSecurityApp/src/main/java/ru/catinasw/springcourse/FirstSecurityApp/services/PeopleService.java
@@ -0,0 +1,22 @@
+package ru.catinasw.springcourse.FirstSecurityApp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.catinasw.springcourse.FirstSecurityApp.models.Person;
+import ru.catinasw.springcourse.FirstSecurityApp.repositories.PeopleRepository;
+
+import java.util.Optional;
+
+@Service
+public class PeopleService {
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PeopleService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    public Optional<Person> loadUserByUsername(String s) {
+        return peopleRepository.findByUserName(s);
+    }
+}
