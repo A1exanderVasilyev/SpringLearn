83e3e568f52dfd94efce5b485905fc3daec47d2e
Catina Swag <catinasw@va-i2.kzn.21-school.ru>
2025-07-17 10:33:30 +0300

    remove AuthProviderImpl(its for custom authentication) and provide details service to security config

---
+++ spring-security/lesson-80/FirstSecurityApp/src/main/java/ru/catinasw/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package ru.catinasw.springcourse.FirstSecurityApp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.catinasw.springcourse.FirstSecurityApp.services.PersonDetailsServiceImpl;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PersonDetailsServiceImpl personDetailsService;
+
+    @Autowired
+    public SecurityConfig(PersonDetailsServiceImpl personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
