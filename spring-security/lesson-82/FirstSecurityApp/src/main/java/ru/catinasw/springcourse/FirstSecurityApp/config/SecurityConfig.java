84900a86703c2b5ee3fea8f3cc99b376766105a8
Catina Swag <catinasw@va-i2.kzn.21-school.ru>
2025-07-17 12:21:42 +0300

    make lesson-82: make registration controller with view

---
+++ spring-security/lesson-82/FirstSecurityApp/src/main/java/ru/catinasw/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package ru.catinasw.springcourse.FirstSecurityApp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.catinasw.springcourse.FirstSecurityApp.services.PersonDetailsServiceImpl;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PersonDetailsServiceImpl personDetailsService;
+
+    @Autowired
+    public SecurityConfig(PersonDetailsServiceImpl personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+    //config spring security
+    //config authorization
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/auth/registration","/error")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
+
+    //config authentication
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
