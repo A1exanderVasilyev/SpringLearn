17e5e523ca04064f4db38373765909e195490ac7
Catina Swag <catinasw@va-i2.kzn.21-school.ru>
2025-07-17 13:21:14 +0300

    make lesson-84: implemented bcrypt for password

---
+++ spring-security/lesson-84/FirstSecurityApp/src/main/java/ru/catinasw/springcourse/FirstSecurityApp/services/RegistrationService.java
@@ -0,0 +1,27 @@
+package ru.catinasw.springcourse.FirstSecurityApp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.catinasw.springcourse.FirstSecurityApp.models.Person;
+import ru.catinasw.springcourse.FirstSecurityApp.repositories.PeopleRepository;
+
+import javax.transaction.Transactional;
+
+@Service
+public class RegistrationService {
+    private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
+        this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public void register(Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
+        peopleRepository.save(person);
+    }
+}
